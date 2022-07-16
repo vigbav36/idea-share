a2c56bbf2e7e3fa613be48e05372d7386ce19b78
vigbav36 <90998381+vigbav36@users.noreply.github.com>
2022-07-16 16:49:31 +0530

    Add files via upload

--- idea/views.py
+++ idea/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from idea.models import Idea
+from users.models import UserAccount
+
 # Create your views here.
+
+def idea_view(request,idea_id):
+    idea = Idea.objects.filter(pk=idea_id)
+    created_user = UserAccount.objects.filter(Username=idea[0].created_by)
+    context={
+        'idea':idea[0],
+        'created_user':created_user[0],
+    }
+    print(context)
+    return render(request,"idea.html",context)
