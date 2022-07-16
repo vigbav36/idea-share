bc0fd3bc44fc70faa4b4d986d529081ea9c8c256
vigbav36 <90998381+vigbav36@users.noreply.github.com>
2022-07-16 09:47:01 +0530

    Add files via upload

--- job/settings.py
+++ job/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'idea',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_dir,'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/" 
